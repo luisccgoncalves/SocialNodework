337c9cdf3a2d6e7a56d1789c64bb8d8d8e0dfbf1
luisccgoncalves <luisccgoncalves@gmail.com>
2018-11-30 19:11:18 +0000

    loading files

--- SocialNodework/SocialNodework/utils.c
+++ SocialNodework/SocialNodework/utils.c
@@ -1,9 +1,40 @@
 #include "utils.h"
 
+int* init_dados(char *nome, int *n, int *iter)
+{
+	FILE    *f;
+	int     *p, *q;
+	int     i, j;
+
+	f = fopen(nome, "r");
+	if (!f)
+	{
+		printf("Erro no acesso ao ficheiro dos dados\n");
+		exit(1);
+	}
+	// Lê o número de iteracoes
+	fscanf(f, " %d", iter);
+	// Lê o número de vértices
+	fscanf(f, " %d", n);
+	// Aloca espaço em memória para guardar a matriz de adjacências
+	p = malloc(sizeof(int)*(*n)*(*n));
+	if (!p)
+	{
+		printf("Erro na alocacao de memoria\n");
+		exit(1);
+	}
+	q = p;
+	// Preenchimento da matriz de adjacências
+	for (i = 0; i < *n; i++)
+		for (j = 0; j < *n; j++)
+			fscanf(f, " %d", q++);
+	fclose(f);
+	// Devolve a matriz de adjacências
+	return p;
+}
+
 void seed_rand() {
-
 	srand((unsigned int)time(NULL));
-
 }
 
 // Gera um valor inteiro aleatório entre min e max
--- SocialNodework/SocialNodework/utils.h
+++ SocialNodework/SocialNodework/utils.h
@@ -1,8 +1,14 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+int* init_dados(char *nome, int *n, int *iter);
+
 void seed_rand();
+int random_l_h(int min, int max);
+float rand_01();
 
 #endif
